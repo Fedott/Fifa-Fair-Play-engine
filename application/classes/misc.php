e26ef25169cbec71fd45b72042f3f3b980652811
Fedot <fedotru@gmail.com>
2010-09-06 02:25:09 +0000

    В хелпер MISC добавлена установка/получение/поверка наличия сообщений об ошибках/успешных действиях.

--- application/classes/misc.php
+++ application/classes/misc.php
@@ -16,4 +16,40 @@
 		{
 			return date('d-m-Y H:i', $date);
 		}
+
+		public static function set_error_message($text)
+		{
+			Session::instance()->set('error_message', $text);
+		}
+
+		public static function get_error_message()
+		{
+			$session = Session::instance();
+			$message = $session->get('error_message');
+			$session->delete('error_message');
+			return $message;
+		}
+
+		public static function isset_error_message()
+		{
+			return (bool) Session::instance()->get('error_message', FALSE);
+		}
+
+		public static function set_apply_message($text)
+		{
+			Session::instance()->set('apply_message', $text);
+		}
+
+		public static function get_apply_message()
+		{
+			$session = Session::instance();
+			$message = $session->get('apply_message');
+			$session->delete('apply_message');
+			return $message;
+		}
+
+		public static function isset_apply_message()
+		{
+			return (bool) Session::instance()->get('apply_message', FALSE);
+		}
 	}
