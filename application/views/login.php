6883f67c6d1d030082569e5c7963b5ed27e0306a
Fedott <fedotru@gmail.com>
2013-03-10 19:35:07 +0300

    Переделана форма логина

--- application/views/login.php
+++ application/views/login.php
@@ -1,40 +1,12 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');?>
-<h2>Авторизация</h2>
-<?if($errors):?>
-<div class="errors">
-	Вы допустили следующие ошибки:
-	<ul>
-		<?foreach($errors as $error):?>
-		<li>
-			<?=$error?>
-		</li>
-		<?endforeach;?>
-</ul>
+<div class="offset4 span3">
+	<form class="form-signin" method="POST">
+		<h2 class="form-signin-heading">Please sign in</h2>
+		<input type="text" class="input-block-level" name="username" placeholder="Логин или email">
+		<input type="password" class="input-block-level" name="password" placeholder="Пароль">
+		<label class="checkbox">
+			<input type="checkbox" value="1" name="remember"> Remember me
+		</label>
+		<button class="btn btn-large btn-primary" type="submit">Sign in</button>
+	</form>
 </div>
-<?endif;?>
-<?=form::open();?>
-<ul>
-	<li class="">
-		<label class="desc" for="username">
-			Логин
-		</label>
-		<div>
-			<input class="field text medium" type="text" name="username" id="username">
-		</div>
-	</li>
-	<li class="">
-		<label class="desc" for="password">
-			Пароль
-		</label>
-		<div>
-			<input class="field text medium" type="password" name="password" id="password">
-		</div>
-	</li>
-	<li>
-		<input type="checkbox" name="remember" value="1" /> Запомнить меня
-	</li>
-	<li>
-		<input type="submit" class="submit" value="Войти">
-	</li>
-</ul>
-<?=Form::close();?>
