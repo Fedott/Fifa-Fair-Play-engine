defdae8aa4969e01ba81651798a8ca985c0bc5d0
Fedott <fedotru@gmail.com>
2013-03-12 01:11:57 +0300

    Добавлены счётчики

--- application/views/fifa.php
+++ application/views/fifa.php
@@ -82,5 +82,33 @@
 			</div>
 		</div>
 	</div>
+
+	<!-- Yandex.Metrika counter -->
+	<div style="display:none;"><script type="text/javascript">
+	(function(w, c) {
+	    (w[c] = w[c] || []).push(function() {
+	        try {
+	            w.yaCounter336263 = new Ya.Metrika(336263);
+	             yaCounter336263.clickmap(true);
+
+	        } catch(e) { }
+	    });
+	})(window, 'yandex_metrika_callbacks');
+	</script></div>
+	<script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript" defer="defer"></script>
+	<noscript><div><img src="//mc.yandex.ru/watch/336263" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
+
+	<!--Google Analytics -->
+	<script type="text/javascript">
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+	try {
+	var pageTracker = _gat._getTracker("UA-5481004-1");
+	pageTracker._trackPageview();
+	} catch(err) {}</script>
+	<!--/Google Analytics -->
 </body>
 </html>
