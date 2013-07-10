0ba4e1ef6d264c6037042f87e0dc6d0f510911a4
Fedot <fedotru@gmail.com>
2013-07-10 17:35:57 +0400

    Правлю тесты

--- tests/acceptance/00004_CreateClubsCept.php
+++ tests/acceptance/00004_CreateClubsCept.php
@@ -24,7 +24,7 @@ $I->see("Аршавин Андрей");
 
 $I->click("Администрирование");
 $I->click("Клубы");
-$I->seeLink("Зенит");
+$I->see("Зенит");
 $I->click("Создать команду");
 $I->fillField("name", "CSKA");
 $I->click("[type=submit]");
